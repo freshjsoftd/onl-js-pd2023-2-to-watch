5ca75091fdf97957933d9f35353d7cd90dc00fc0
yan <yan.braverman37@gmail.com>
2024-04-27 12:54:15 +0300

    Add store, reducer and actions

---
+++ src/store/actions/movieActions.js
@@ -0,0 +1,28 @@
+
+export const addMovie = (movie) => {
+    return {
+        type: 'addMovie',
+        payload: movie,
+    }
+}
+
+export const delMovie = (id) => {
+    return {
+        type: 'deleteMovie',
+        payload: id,
+    }
+}
+
+export const toggleMovie = (id) => {
+    return {
+        type: 'toggleMovie',
+        payload: id,
+    }
+}
+
+export const getMovies = (movies) => {
+    return {
+        type: 'getMovies',
+        payload: movies
+    }
+}
---
+++ src/store/reducers/moviesReducer.js
@@ -0,0 +1,22 @@
+import {moviesState} from '../../model/initialMovies';
+
+const initialState = {
+    movies: moviesState,
+}
+
+export default function moviesReducer(state = initialState, {type, payload}){
+
+    switch(type){
+        case 'addMovie': 
+            return {...state, movies: [...state.movies, payload]};
+        case 'deleteMovie':
+            return {...state, movies: state.movies.filter((movie) => movie.id !== payload)};
+        case 'toggleMovie': 
+            return {...state, movies: state.movies.map((movie) => movie.id === payload
+                 ? {...movie, isDone: !movie.isDone}
+                : movie)};
+        case 'getMovies': 
+            return {...state, movies: payload}
+        default: return state;
+    }
+}
