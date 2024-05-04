9ee4f8eaafbd1fe89324ccbc5ddb64c28e221780
yan <yan.braverman37@gmail.com>
2024-05-04 14:06:49 +0300

    Add saga for getting and creating movies

--- src/store/reducers/moviesReducer.js
+++ src/store/reducers/moviesReducer.js
@@ -3,28 +3,48 @@ import ACTION_TYPES from '../actions/actionTypes';
 
 const initialState = {
 	movies: moviesState,
+	isFetching: false,
+	error: null,
 };
 
 export default function moviesReducer(state = initialState, { type, payload }) {
 	switch (type) {
-		case ACTION_TYPES.ADD_MOVIE:
-			return { ...state, movies: [...state.movies, payload] };
-		case ACTION_TYPES.DELETE_MOVIE:
+		// Success
+		case ACTION_TYPES.POST_MOVIE_SUCCESS:
+			return {
+				...state,
+				movies: [...state.movies, payload],
+				isFetching: false,
+			};
+		case ACTION_TYPES.DELETE_MOVIE_SUCCESS:
 			return {
 				...state,
 				movies: state.movies.filter((movie) => movie.id !== payload),
+				isFetching: false,
 			};
-		case ACTION_TYPES.TOGGLE_MOVIE:
+		case ACTION_TYPES.PUT_MOVIE_SUCCESS:
 			return {
 				...state,
 				movies: state.movies.map((movie) =>
-					movie.id === payload
+					movie.id === payload.id
 						? { ...movie, isDone: !movie.isDone }
 						: movie
 				),
+				isFetching: false,
 			};
-		case ACTION_TYPES.GET_MOVIES:
-			return { ...state, movies: payload };
+		case ACTION_TYPES.GET_MOVIES_SUCCESS:
+			return { ...state, movies: payload, isFetching: false };
+		// Requsting
+		case ACTION_TYPES.POST_MOVIE_REQUEST:
+		case ACTION_TYPES.DELETE_MOVIE_REQUEST:
+		case ACTION_TYPES.PUT_MOVIE_REQUEST:
+		case ACTION_TYPES.GET_MOVIES_REQUEST:
+			return { ...state, isFetching: true };
+		case ACTION_TYPES.POST_MOVIE_ERROR:
+		case ACTION_TYPES.DELETE_MOVIE_ERROR:
+		case ACTION_TYPES.PUT_MOVIE_ERROR:
+		case ACTION_TYPES.GET_MOVIES_ERROR:
+			return {...state, isFetching: false, error: payload}
 		default:
 			return state;
 	}
